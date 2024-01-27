cfd6642df6c7e4ce04c93c511ed4341038880dd7
Tyler Chen <tyche4490@gmail.com>
2024-01-27 17:36:34 -0500

    added database apis folder

---
+++ site/apis/database.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from dotenv import find_dotenv, load_dotenv
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+KINTONE_API = os.environ.get("KINTONE_TOKEN")
+"""
+Function to add a user to the database.
+"""
+def create_database_user(user_id):
+    pass
+
+"""
+Retrieves user from database.
+Parameters: user Auth0 unique ID.
+Returns: dict in following format:
+{
+    "spotify-token": "SPOTIFY_TOKEN",
+    "playlists": {
+        "playlist_id": "PLAYLIST_ID",
+        ...
+    }
+
+}
+"""
+def get_database_user_info(user_id):
+    pass
+
+"""
+
+"""
+def create_database_playlist(user_id):
+    pass
+
+def get_database_playlist(playlist_id):
+    pass
+
+
