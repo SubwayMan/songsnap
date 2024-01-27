9507b147609ec4a78bf6009b9e8de3000bd9170a
Tyler Chen <tyche4490@gmail.com>
2024-01-27 11:43:50 -0500

    added template example

--- site/main.py
+++ site/main.py
@@ -8,7 +8,7 @@ def homepage():
 
 @app.route("/login")
 def login_page():
-    return "Login"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
