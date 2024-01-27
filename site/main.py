fd9865c64cfd776c5141379797600dc08741e6fa
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-01-27 16:08:09 -0500

    fixed main and added upload file

--- site/main.py
+++ site/main.py
@@ -10,5 +10,17 @@ def homepage():
 def login_page():
     return render_template("index.html")
 
+@app.route("/albums")
+def albums():
+    return render_template("albums.html")
+
+@app.route("/album")
+def album_single():
+    return render_template("album_single.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
