e0ebd1a1900a699bb744c6ce175947a77633e049
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-01-27 00:02:26 -0500

    vision is working

--- pipelines/vision.py
+++ pipelines/vision.py
@@ -1,16 +1,37 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = OpenAI()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client.api_key = os.getenv("OPENAI_API_KEY")
 
-def describe_image(filePath):
-    return 0
+def describe_image(img_url):
+    response = client.chat.completions.create(
+                    model="gpt-4-vision-preview",
+                    messages=[
+                        {
+                          "role": "user",
+                          "content": [
+                            {"type": "text", "text": "Describe this image. Make sure to include what timeframe and culture it looks like its from, as well as the mood of the image overall image"},
+                            {
+                              "type": "image_url",
+                              "image_url": {
+                                "url": img_url,
+                              },
+                            },
+                          ],
+                        }
+                    ],
+                    max_tokens=300,
+    )
 
-current_dir = os.getcwd()
-img_folder = os.path.join(current_dir, "Images")
-img_path = os.path.join(img_folder, "img1.jpg")
+    print(response)
 
-image_desc = describe_image(img_path)
+    return response.choices[0]
+
+img_url = 'https://www.trumanlibrary.gov/sites/default/files/styles/tiff_conversion/public/photographs/2016/2016-266.tif.jpg?itok=75tRRJY5'
+image_desc = describe_image(img_url)
 
 print(image_desc)
