fab2e62fefb6ebaa400f82a872ac01d326b27643
KayakKayla <137660752+KayakKayla@users.noreply.github.com>
2024-01-27 21:59:15 -0500

    Redirected automatically to login, removed index.html, and edited the css

--- site/main.py
+++ site/main.py
@@ -36,19 +36,15 @@ def homepage():
 
 @app.route("/login")
 def login():
-    return render_template("index.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
 
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
     token = oauth.auth0.authorize_access_token()
     session["user"] = token
     return redirect("/")
-
-@app.route("/loginform")
-def login_page():
-    return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
-    )
 
 @app.route("/logout")
 def logout():
