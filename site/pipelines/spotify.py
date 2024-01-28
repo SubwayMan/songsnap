090a067ada419a6b3c854833d8509df48b753c9c
Tyler Chen <tyche4490@gmail.com>
2024-01-28 00:05:52 -0500

    banished taipy to the shadow realm

--- site/pipelines/spotify.py
+++ site/pipelines/spotify.py
@@ -28,28 +28,6 @@ def get_token():
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
-# def search_for_artist(token, artist_name):
-#     url = "https://api.spotify.com/v1/search"
-#     headers = get_auth_header(token)
-#     query = f"?q={artist_name}&type=artist&limit=1"
-
-#     query_url = url + query
-#     result = get(query_url, headers=headers)
-#     json_result = json.loads(result.content)["artists"]["items"]
-#     if len(json_result) == 0:
-#         print("No artist with this name exists...")
-#         return None
-    
-#     return json_result[0]
-#     # print(json_result)
-
-# def get_songs_by_artist(token, artist_id):
-#     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-#     headers = get_auth_header(token)
-#     result = get(url, headers=headers)
-#     json_result = json.loads(result.content)["tracks"]
-#     return json_result
-
 def get_link(token, name_and_artist):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
